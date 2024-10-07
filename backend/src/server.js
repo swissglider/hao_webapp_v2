1b4b1ea316bb7ddf11eac9987d1d1161a94f4d8b
swissglider <npm@mailschweiz.com>
2024-10-07 12:18:44 +0200

    Aktualisiere devcontainer.json mit neuen Erweiterungen und Einstellungen

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const axios = require("axios");
-const cors = require('cors');
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
@@ -14,16 +14,26 @@ app.get("/api/ha", async (req, res) => {
     const response = await axios.get(`${process.env.HA_URL}/api/`, {
       headers: { Authorization: `Bearer ${process.env.HA_TOKEN}` },
     });
-    console.log("Erfolgreiche Antwort:", JSON.stringify(response.data, null, 2)); // Erfolgsfall
+    console.log(
+      "Erfolgreiche Antwort:",
+      JSON.stringify(response.data, null, 2)
+    ); // Erfolgsfall
     res.json(response.data);
   } catch (error) {
-    console.error("Fehler bei der Anfrage:", JSON.stringify(error.response ? error.response.data : error.message, null, 2)); // Fehlerfall
+    console.error(
+      "Fehler bei der Anfrage:",
+      JSON.stringify(
+        error.response ? error.response.data : error.message,
+        null,
+        2
+      )
+    ); // Fehlerfall
     res.status(500).json({ error: "Failed to fetch data from Home Assistant" });
   }
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
